5774d8092195bca1cdadb65211faf6c2ffdcd4ca
Joris Leermakers <jorisleermakers@users.noreply.github.com>
2025-01-31 15:16:19 +0100

    Update MapboxMapsPlugin.m

--- ios/Classes/MapboxMapsPlugin.m
+++ ios/Classes/MapboxMapsPlugin.m
@@ -1,5 +1,5 @@
 #import "MapboxMapsPlugin.h"
-#import <mapbox_gl/mapbox_gl-Swift.h>
+#import <flutter_mapbox_gl_safaribuddy/mapbox_gl-Swift.h>
 
 @implementation MapboxMapsPlugin 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
